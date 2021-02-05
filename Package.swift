be82ea9e5c108455188ba812c05c07cc17ca567e
Oleksandr Orlov <oleksandr.orlov@cgm.com>
2021-02-05 18:35:31 +0200

    Added AnyFormatKit as dependency to Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,23 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "AnyFormatKitSwiftUI",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "AnyFormatKitSwiftUI",
             targets: ["AnyFormatKitSwiftUI"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/luximetr/AnyFormatKit", .upToNextMajor(from: "2.4.0"))
+    ],
     targets: [
         .target(
             name: "AnyFormatKitSwiftUI",
+            dependencies: ["AnyFormatKit"],
             path: "Source"
         )
     ]
