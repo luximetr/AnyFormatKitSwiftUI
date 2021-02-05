d219932216e7f770a38afc748a1860ed6f908d82
Oleksandr Orlov <oleksandr.orlov@cgm.com>
2021-02-05 22:05:07 +0200

    Exclude file.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
             name: "AnyFormatKitSwiftUI",
             dependencies: ["AnyFormatKit"],
             path: "Sources",
-            exclude: ["./../Example", "README.md", "ExcludeFile.swift"]),
+            exclude: ["./../Example", "README.md"]),
         .testTarget(
             name: "AnyFormatKitSwiftUITests",
             dependencies: ["AnyFormatKitSwiftUI"]),
