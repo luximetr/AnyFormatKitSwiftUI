badf7ba86872cb126032a08fd6878cf1f67d7451
Oleksandr Orlov <oleksandr.orlov@cgm.com>
2021-02-05 19:05:55 +0200

    Updated package.swift and xcodeproj

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
 // swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -6,19 +7,24 @@ let package = Package(
     name: "AnyFormatKitSwiftUI",
     platforms: [.iOS(.v13)],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AnyFormatKitSwiftUI",
-            targets: ["AnyFormatKitSwiftUI"]
-        )
+            targets: ["AnyFormatKitSwiftUI"]),
     ],
     dependencies: [
         .package(url: "https://github.com/luximetr/AnyFormatKit", .upToNextMajor(from: "2.4.0"))
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AnyFormatKitSwiftUI",
             dependencies: ["AnyFormatKit"],
-            path: "Source"
-        )
+            path: "Sources"),
+        .testTarget(
+            name: "AnyFormatKitSwiftUITests",
+            dependencies: ["AnyFormatKitSwiftUI"],
+            path: "Tests"),
     ]
 )
