93daa5c0f1ddf19c70e5201d9e71a854f80b1130
Oleksandr Orlov <oleksandr.orlov@cgm.com>
2021-02-05 20:43:22 +0200

    Moved source code to Sources folder.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AnyFormatKitSwiftUI",
-            dependencies: ["AnyFormatKit"]),
+            dependencies: ["AnyFormatKit"],
+            path: "Sources"),
         .testTarget(
             name: "AnyFormatKitSwiftUITests",
             dependencies: ["AnyFormatKitSwiftUI"]),
