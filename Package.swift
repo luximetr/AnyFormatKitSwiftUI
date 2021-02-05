0905ce323be60bbd4e4118dbcecd995a7d034cb4
Oleksandr Orlov <oleksandr.orlov@cgm.com>
2021-02-05 20:08:58 +0200

    Added AnyFormatKit dependency to 'AnyFormatKitSwiftUI' target.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AnyFormatKitSwiftUI",
-            dependencies: []),
+            dependencies: ["AnyFormatKit"]),
         .testTarget(
             name: "AnyFormatKitSwiftUITests",
             dependencies: ["AnyFormatKitSwiftUI"]),
