0ee6a6b1070975a1fa80d9d22b720b122090cad8
aleksandr.orlov <aleksandr.orlov@deskera.com>
2021-04-10 21:41:23 +0800

    Added platforms to Package.swift. Updated AnyFormatKit dependency version.

--- Package.swift
+++ Package.swift
@@ -5,18 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "AnyFormatKitSwiftUI",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AnyFormatKitSwiftUI",
             targets: ["AnyFormatKitSwiftUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/luximetr/AnyFormatKit", .upToNextMajor(from: "2.4.0"))
+        .package(url: "https://github.com/luximetr/AnyFormatKit", .upToNextMajor(from: "2.5.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AnyFormatKitSwiftUI",
             dependencies: ["AnyFormatKit"],
